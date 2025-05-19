3d23266a9b281b1188583bbb962ee799de0d0537
osegdev <omarosegdev@gmail.com>
2025-05-19 17:40:14 -0600

    feat: added console logging

--- app/interfaces/api.py
+++ app/interfaces/api.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, Query
 from typing import List
 from app.use_cases.search_engine import SearchEngine
 from app.use_cases.boolean_search import BooleanSearch
 from app.domain.index import InvertedIndex
 from app.infrastructure.document_loader import DocumentLoader
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, Query, HTTPException
+from loguru import logger
 import re
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+from fastapi import status
 
 
 app = FastAPI(title="Simple Search Engine API")
@@ -23,9 +26,15 @@ engine = SearchEngine(BooleanSearch(index))
 @app.get("/search")
 def search(q: str = Query(..., min_length=2, max_length=100)):
     clean_q = re.sub(r"[^a-zA-Z0-9áéíóúüñ\s]", "", q.lower().strip())
+
     if not clean_q:
-        raise HTTPException(status_code=400, detail="Consulta inválida.")
-    return engine.search(clean_q)
+        logger.warning("Invalid query: '%s'", q)
+        raise HTTPException(status_code=400, detail="Invalid query")
+    
+    logger.info("Consulta recibida: '%s'", clean_q)
+    results = engine.search(clean_q)
+    logger.info("Resultados encontrados: %s", results)
+    return results
 
 app.add_middleware(
     CORSMiddleware,
@@ -34,3 +43,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Error inesperado: {exc}")
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"detail": "Error interno del servidor"},
+    )
